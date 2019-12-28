a87c50fdfa77315fd231df5a738648b012f5ffaf
admin <admin@qq.com>
2019-12-28 17:16:40 +0800

    提交HelloWorld工程

---
+++ git-test-project/src/com/itcast/git/firstGit.java
@@ -0,0 +1,13 @@
+package com.itcast.git;
+
+public class firstGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git.......");
+        System.out.println("hello git.......");
+        System.out.println("hello git.......");
+        System.out.println("hello git.......");
+        System.out.println("hello git.......");
+
+    }
+}
